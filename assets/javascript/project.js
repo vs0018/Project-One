ef4e7ae138b92fdf5bde50290abba258856a1532
Valerie <43964677+vs0018@users.noreply.github.com>
2019-01-05 10:41:10 -0500

    Feature/index build (#35)
    * add UIKit Lightbox html without initialization
    
    * add js to append image div(lightbox)
    
    * fix lightbox code
    
    * attempt auto launch of lightbox
    
    * add for loop to generate 5 locations
    
    * add login card html
    
    * change login to modal but without autolaunch

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -20,6 +20,9 @@ $(document).ready(function () {
     // encodeURI() : for be able to consider the spaces for the list in words 
 
     function getLocationImage(location) {
+
+    var i;
+    for (i = 0; i < 6; i++) {  
         var location = vacations[Math.floor(Math.random() * vacations.length)];
         console.log("random location is : " + location);
         var access_key = '8a76ab2b11add964b157a4e5b50af0f90aa2d0ff072a5cd4d0688af17224d0c2'
@@ -34,12 +37,43 @@ $(document).ready(function () {
             dataType: 'json',
         })
             .done(function (response) {
+                console.log(response);
                 console.log(response.description);
+ 
+                console.log(response.location.name);
+
                 console.log(response.location);
                 var src = response.urls.regular;
-                $('#image').html('<img src= ' + src + "/>");
-                $('#location-name').html("<h1>" + location + "</h1>");
+                // $('#image').html('<img src= ' + src + "/>");
+                // $('#location-name').html("<h1>" + location + "</h1>");
+
+                var anchor = document.createElement("a");
+
+                var link = document.createAttribute("href");
+                link.value = src;
+    
+                var type = document.createAttribute("data-type");
+                type.value = "image";
+
+                var image = document.createElement("img");
+
+                var picture = document.createAttribute("src");
+                picture.value = src;
+
+                anchor.setAttributeNode(link);
+                anchor.setAttributeNode(type);
+                image.setAttributeNode(picture);
+
+                anchor.appendChild(image);
+
+                var div = document.getElementById('locations');
+            
+                div.appendChild(anchor);
+            
             })
+ feature/index-build
+        };
+
             var key = '11184612-ba39f46d40d6f65f978010ca1';
             var url = 'https://pixabay.com/api/?key=' + key + '&q=' +location + '&image_type=photo&per_page=6'
             $.ajax({
@@ -69,11 +103,18 @@ $(document).ready(function () {
         
         
             })
+ master
     }
 
 
 
     getLocationImage();
+ feature/index-build
+
+    $("#myModal").show();
+})
+
+master
 
 
 
